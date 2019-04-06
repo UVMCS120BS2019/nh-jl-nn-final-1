d9570e94d21b01524710a421d98110b0594c92ba
Nicholas Hanoian <nicholashanoian@gmail.com>
2019-04-06 17:22:13 -0400

    pong files

---
+++ src/pong.cpp
@@ -0,0 +1,19 @@
+#include "pong.h"
+
+
+void Pong::drawStart() {
+	
+}
+
+void Pong::drawGame() {
+	
+}
+
+void Pong::drawEnd() {
+	
+}
+
+
+void Pong::timestep() {
+	
+}
---
+++ src/pong.h
@@ -0,0 +1,26 @@
+#ifndef PONG_H
+#define PONG_H
+
+#include "rect.h"
+#include "circle.h"
+#include "graphics.h"
+
+class Pong {
+	Rect userPaddle;
+	Rect cpuPaddle;
+	Circle ball;
+	int userScore;
+	int cpuScore;
+
+	void drawStart();
+	void drawGame();
+	void drawEnd();
+
+	void timestep();
+
+	
+	
+}
+
+
+#endif
